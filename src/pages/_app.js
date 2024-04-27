c302663eb588cc72b490a29fe80ef26302eb2fe5
alireza <alirezanghngh1379@gmail.com>
2024-04-27 22:06:47 +0430

    set-footer-template

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/sharedUi/Footer";
 import NavBar from "@/components/sharedUi/NavBar";
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }) {
     <NextUIProvider>
       <NavBar />
       <Component {...pageProps} />
+      <Footer/>
     </NextUIProvider>
   );
 }
